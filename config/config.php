02235f6cd38a91dc9e35ddc91e71f5cc73f12146
Asafeist <rizky.nurahfiizh@gmail.com>
2025-07-26 15:56:00 +0700

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+try {
+
+    //host
+    define("HOST", "localhost");
+
+    //dbname
+    define("DBNAME", "coffee-blend");
+
+    //user
+    define("USER", "root");
+
+    //pass
+    define("PASS", "");
+
+    $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $Exception) {
+
+    echo $Exception->getMessage();
+}
+    // if($conn == true) {
+    //     echo "connection is fine";
+    // }else {
+    //     echo "error";
+    // }
